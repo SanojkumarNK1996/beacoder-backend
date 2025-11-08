03cd4ffccab838fcbfd613b48be26829aa69120c
Akshay <akshay.kumar@fortunesoftit.com>
2025-11-08 15:26:25 +0530

    profile-api-adding

--- source/routes/user.routes.js
+++ source/routes/user.routes.js
@@ -1,3 +1,5 @@
+const { requireAuth } = require('../middleware/authorize');
+
 const router = require('express').Router();
 
 router.get("/", (req, res) => {
@@ -8,9 +10,9 @@ router.get("/", (req, res) => {
     }
 })
 
-router.get("/profile", async (req, res) => {
+router.get("/profile", requireAuth, async (req, res) => {
     try {
-        const { name } = "user"
+        const { name } = req.user
         return res.status(200).json({ msg: "user fetched succesfully", userName: name })
     } catch (error) {
         return res.status(500).json({ error: error.message })
